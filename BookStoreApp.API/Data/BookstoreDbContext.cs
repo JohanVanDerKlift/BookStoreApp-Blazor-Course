f16013fc2ea8a151da40ff6c180736bcb15aa452
JohanVanDerKlift <info@johanvanderklift.nl>
2024-01-08 21:27:24 +0100

    Added Ef Core and Scaffolded Database

---
+++ BookStoreApp.API/Data/BookstoreDbContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreApp.API.Data;
+
+public partial class BookstoreDbContext : DbContext
+{
+    public BookstoreDbContext()
+    {
+    }
+
+    public BookstoreDbContext(DbContextOptions<BookstoreDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Author> Authors { get; set; }
+
+    public virtual DbSet<Book> Books { get; set; }
+
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Author>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Authors__3214EC07FC4611DD");
+
+            entity.Property(e => e.Bio).HasMaxLength(250);
+            entity.Property(e => e.FirstName).HasMaxLength(50);
+            entity.Property(e => e.LastName).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Book>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Books__3214EC07E20A8F10");
+
+            entity.HasIndex(e => e.Isbn, "UQ__Books__447D36EAF7114513").IsUnique();
+
+            entity.Property(e => e.Image).HasMaxLength(50);
+            entity.Property(e => e.Isbn)
+                .HasMaxLength(50)
+                .HasColumnName("ISBN");
+            entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.Summary).HasMaxLength(250);
+            entity.Property(e => e.Title).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
